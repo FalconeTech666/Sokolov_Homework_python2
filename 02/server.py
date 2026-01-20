737abf568bdc1aa5c6f5243a8520dd30f944318e
Falke666 <sokolovmslu@gmail.com>
2026-01-20 15:25:14 +0300

    2

--- 02/server.py
+++ 02/server.py
@@ -45,3 +45,148 @@
                  
 
 '''
+
+
+import socket
+
+def send_file(file_name, conn):
+    try:
+        with open(file_name.lstrip('/'), 'rb') as f:                   
+            print(f"send file {file_name}")
+            conn.send(OK)
+            conn.send(HEADERS)
+            conn.send(f.read())
+            
+    except IOError:
+        print('нет файла')
+        conn.send(ERR_404)
+
+def is_file(path):       
+    if '.' in path:
+        ext =  path.split(".")[-1]
+        if ext in ['jpg','png','gif', 'ico', 'txt', 'html', 'json']:
+            return True
+    return False
+
+
+def parse_command(line):
+    parts = [p.strip() for p in line.split(';')]
+    cmd_part = parts[0].split(':', 1)     
+    login_part = parts[1].split(':', 1)   
+    pass_part = parts[2].split(':', 1)    
+
+    command = cmd_part[1]
+    login = login_part[1]
+    password = pass_part[1]
+    return command, login, password
+
+def valid_login(login: str) -> bool:
+    return len(login) >= 6 and login.isalnum()
+
+def valid_password(password: str) -> bool:
+    return len(password) >= 8 and any(ch.isdigit() for ch in password)
+
+HOST = ("0.0.0.0", 7777)  
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(HOST)
+sock.listen()
+
+print("----СТАРТ-----")
+
+
+OK = b'HTTP/1.1 200 OK\n'
+HEADERS = b"Host: some.ru\nHost1: some1.ru\nContent-Type: text/html; charset=utf-8\n\n"
+ERR_404 = b'HTTP/1.1 404 Not Found\n\n'
+
+users = {}
+
+while 1:
+    print('----------------- listen ------------------')
+    conn, addr = sock.accept()
+
+    try:
+        data = conn.recv(4096)
+        text = data.decode("utf-8", errors="ignore")
+        print(text)
+
+        first_line = text.split('\n', 1)[0].strip()
+
+        if first_line.startswith('GET ') or first_line.startswith('POST '):
+            method, path, ver = first_line.split(" ", 2)
+            print('HTTP:', method, path, ver)
+
+            if is_file(path):
+                send_file(path, conn)
+
+            else:
+                if path == '/':
+                    send_file('1.html', conn)
+
+                elif path.startswith('/test/') and path.endswith('/'):
+                    num_str = path[len('/test/'):-1]
+                    if num_str.isdigit():
+                        html = f"<h1>ТЕСТ {num_str} запущен</h1>"
+                        conn.send(OK)
+                        conn.send(HEADERS)
+                        conn.send(html.encode())
+                    else:
+                        conn.send(ERR_404)
+
+                elif path.startswith('/message/'):
+                    parts = path.strip('/').split('/') 
+                    if len(parts) >= 3:
+                        login = parts[1]
+                        text_msg = '/'.join(parts[2:])
+                        
+                        from datetime import datetime
+                        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        
+                        msg = f"{now} - сообщение от пользователя {login} - {text_msg}"
+                        print(msg)                     
+                        
+                        conn.send(OK)
+                        conn.send(HEADERS)
+                        conn.send(msg.encode('utf-8')) 
+                    else:
+                        conn.send(ERR_404)
+                else:
+                    msg = f"пришли неизвестные данные по HTTP - путь {path}"
+                    conn.send(OK)
+                    conn.send(HEADERS)
+                    conn.send(msg.encode())
+        else:
+            line = text.strip()
+
+            if line.startswith("command:reg"):
+                cmd, login, password = parse_command(line)
+                from datetime import datetime
+                now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                if valid_login(login) and valid_password(password):
+                    users[login] = password
+                    msg = f"{now} - пользователь {login} зарегистрирован"
+
+                else:
+                    msg = f"{now} - ошибка регистрации {login} - неверный пароль/логин"
+
+                    conn.send(msg.encode('utf-8'))
+
+            elif line.startswith("command:signin"):
+                cmd, login, password = parse_command(line)
+                from datetime import datetime
+                now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                if login in users and users[login] == password:
+                    msg = f"{now} - пользователь {login} произведен вход"
+                else:
+                    msg = f"{now} - ошибка входа {login} - неверный пароль/логин"
+
+                conn.send(msg.encode('utf-8'))
+            else:
+                print("NOT HTTP, unknown:", line)
+
+    except Exception as e:
+        print("ERROR:", e)
+
+    conn.close()
